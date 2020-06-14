d09801b24ae3c12652f2e983fd3be9eb26d26c08
Jerry <jerry_techtree@126.com>
2020-06-14 23:56:56 +0800

    Updated 更新镜像构建方法
    Signed-off-by: Jerry <jerry_techtree@126.com>

--- app/common-component/clone.sh
+++ app/common-component/clone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEPENDENCES=(laravel-account laravel-ad laravel-address laravel-appmanager laravel-article laravel-attachment laravel-audit laravel-base laravel-comment laravel-district laravel-endlesslevel laravel-favorite laravel-goods laravel-location laravel-message laravel-order laravel-payment laravel-recharge laravel-statistics laravel-sysconfig laravel-user location state-machine)
+DEPENDENCES=(laravel-account laravel-activity laravel-ad laravel-address laravel-agent laravel-appmanager laravel-article laravel-attachment laravel-audit laravel-base laravel-comment laravel-district laravel-endlesslevel laravel-favorite laravel-goods laravel-im laravel-location laravel-message laravel-order laravel-payment laravel-recharge laravel-shop laravel-statistics laravel-sysconfig laravel-user location state-machine)
 
 for dep in ${DEPENDENCES[@]}
 do
